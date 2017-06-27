37e8a261790ef8c885f2fa322a1725972ea32692
Mark Gill <mark.gill@orionhealth.com>
2017-06-28 10:55:52 +1200

    Generate fhir patient using passed in fields

--- src/resources/dstu2/patient.js
+++ src/resources/dstu2/patient.js
@@ -10,20 +10,21 @@ import AdministrativeGender from '../../datatypes/dstu2/AdministrativeGender';
 import ContactBackboneElement from '../../datatypes/dstu2/ContactBackboneElement';
 
 const generators = {
-  id: faker.random.uuid(),
-  identifier: Identifier(),
-  active: faker.random.boolean(),
-  name: HumanName(),
-  telecom: ContactPoint(),
-  gender: AdministrativeGender(),
-  birthDate: moment(faker.date.past(100)).format('YYYY-MM-DD'),
-  address: Address(),
-  contact: ContactBackboneElement(),
+  id: () => faker.random.uuid(),
+  identifier: () => Identifier(),
+  active: () => faker.random.boolean(),
+  name: () => HumanName(),
+  telecom: () => ContactPoint(),
+  gender: () => AdministrativeGender(),
+  birthDate: () => moment(faker.date.past(100)).format('YYYY-MM-DD'),
+  address: () => Address(),
+  contact: () => ContactBackboneElement(),
 };
 
 const patient = R.compose(
   R.merge({ resourceType: 'Patient' }),
-  R.map(key => ({ [key]: generators[key] })),
+  R.evolve(generators),
+  R.zipObj(R.__, R.repeat(undefined, 3)), // eslint-disable-line no-underscore-dangle
 );
 
 export default patient;
