75afdd3236d68071c6c924e33e9d90dae7374c83
Mark Gill <markgillnz@gmail.com>
2017-07-01 22:29:59 +1200

    Refactor develop script

---
+++ develop.js
@@ -0,0 +1,5 @@
+/* eslint-disable no-console */
+import generator from './src';
+
+console.log(JSON.stringify(generator.dstu2.patient(), null, 2));
+console.log(JSON.stringify(generator.dstu2.allergyIntolerance(), null, 2));
