7aa74a5b65e17c941da94ba32fe1f35f6d8db85b
Mark Gill <markgillnz@gmail.com>
2017-07-08 22:46:46 +1200

    Fix ordering of inner extensions

--- src/datatypes/dstu2/extensions/InnerExtension.js
+++ src/datatypes/dstu2/extensions/InnerExtension.js
@@ -4,7 +4,7 @@ import extension from '../../../resources/dstu2/extension';
 
 const InnerExtension = fields => ({
   url: faker.internet.url(),
-  entension: extension(fields),
+  extension: extension(fields),
 });
 
 export default InnerExtension;
--- src/resources/dstu2/extension.js
+++ src/resources/dstu2/extension.js
@@ -4,14 +4,18 @@ import applyOverrides from '../../utils/applyOverrides';
 import { transformer, availableFields } from '../../transformers/dstu2/extension';
 
 const extension = (fields, overrides) => R.compose(
+  applyOverrides(overrides),
   R.flatten,
   R.values,
-  applyOverrides(overrides),
   R.mapObjIndexed(transformer),
   R.ifElse(
     R.isNil,
     () => R.zipObj(availableFields, R.repeat(1, availableFields.length)),
-    R.pick(availableFields.concat('extension')),
+    R.compose(
+      R.fromPairs,
+      R.filter(R.apply(key => R.contains(key, availableFields.concat('extension')))),
+      R.toPairs,
+    ),
   ),
 )(fields);
 
